7c9e4b17b92e9fbc9ca7171bb3dd18196ed496c5
smos86 <86353676+smos86@users.noreply.github.com>
2021-07-06 14:06:18 +0300

    06_07_21
    Изменение класса в элементе getElementsByClassName

--- Bot_for_Yandex.js
+++ Bot_for_Yandex.js
@@ -12,12 +12,12 @@ let keywords = ["Купить новый автомобиль", "Отзывы а
 let keyword = keywords[getRandom(0,keywords.length)];
 
 document.getElementsByName("text")[0].value = keyword;
-let button = document.getElementsByClassName('//button[contains (@class, "mini-suggest__button")]')[0];
+let button = document.getElementsByClassName('button_theme_search')[0];
 let links = document.links;
 
 if (button !== undefined) {
     document.getElementsByName("text")[0].value = keyword;
-    document.getElementsByClassName('//button[contains (@class, "mini-suggest__button")]')[0].click();
+    document.getElementsByClassName('button_theme_search')[0].click();
 }else{
 for (let i=0; i<links.length; i++) {
 if (links[i].href.includes("auto.ru")) {
