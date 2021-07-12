7ab48096831bdb05250eeea5f389364934e5503e
smos86 <86353676+smos86@users.noreply.github.com>
2021-07-13 00:12:25 +0300

    13_07_21
    Работа с неограниченным кол-вом сайтов и поисковых запросов посредством поиска Google.  Принятие Cookie от сайтов и работа с ними.

--- Bot_for_Google.js
+++ Bot_for_Google.js
@@ -1,31 +1,50 @@
 // ==UserScript==
-// @name         Google Bot
+// @name         Google Bot v.3
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       Smogunov Sergey
 // @match        https://www.google.com/*
 // @match        https://napli.ru/*
+// @match        https://psyholog.me/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-// ключевые слова:
-let keywords = ["DevTools — очень полезная штука для разработчика",
-       "Редакции — это резервные копии",
-    "Google Fonts очень популярны",];
+// Целевые сайты и ключи:
+let sites = {
+    "napli.ru":['вывод произвольных полей wordpress', 'Отключение редакций и ревизий в WordPress', '10 самых популярных шрифтов от Google'],
+    "psyholog.me":['центр здоровых отношений "Запятая"', 'Услуги центра здоровых отношений', 'Чекалина Елена психолог'],
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":['как звучит кларнет', 'как звучит гобой', 'Музыкальные диктанты']
+};
+//Переменная с целевыми сайтами, рандомный запрос:
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+// ключи:
+let keywords = sites[site];
 
 // Определение кнопки "Найти" и написание рандомного запроса по ключевым словам:
 let googleInput = document.getElementsByName("q")[0];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-// Здаание переменной кнопки "Найти":
+// Задание переменной кнопки "Найти":
 let btnK = document.getElementsByName("btnK")[0];
 let links = document.links;
 let i = 0;
 
+ //сбор куки на главной странице:
 if (btnK !== undefined) {
+    document.cookie = `site = ${site}`;
+}else if(location.hostname == "www.google.com"){
+    site = getCookie("site");
+}else{
+    site = location.hostname;
+}
+
+
+if (btnK !== undefined) {
+    document.cookie = `site = ${site}`;
     // написание запроса по одной букве с времени 200 мс:
-   let timerId = setInterval(() => {
+    let timerId = setInterval(() => {
         googleInput.value += keyword[i];
         i++;
         if (i == keyword.length) {
@@ -34,50 +53,43 @@ if (btnK !== undefined) {
         }
     }, 200);
 
-// Указание сайта в котором необходимо задать переход по ссылкам:
-} else if(location.hostname == "napli.ru") {
-    console.log("Мы на Napli!")
+    // Указание сайта в котором необходимо задать переход по ссылкам:
+} else if(location.hostname == site) {
+    console.log("Мы на Сайте")
 
-// Функция перехода по ссылкам внутри указанного сайта и возврата на главную страницу по определённому интервалу:
+    // Функция перехода по ссылкам внутри указанного сайта и возврата на главную страницу по определённому интервалу:
     setInterval(()=>{
-    let index = getRandom(0,links.length);
-    if(getRandom(0,101)>=70) {
-    location.href = "https://www.google.com";
-    }
-    else if (links[index].href.indexOf("napli.ru") !== -1)
-    links[index].click();
+        let index = getRandom(0,links.length);
+        if(getRandom(0,101)>=80) {
+            location.href = "https://www.google.com";
+        }
+        else if (links[index].href.indexOf(site) !== -1)
+            links[index].click();
     }, getRandom(1000,5000));
 
     // Если на первой страницы не найдена ключевая фраза, идти дальше:
 } else {
     let nextGooglePage = true;
     for (let i = 0; i < links.length; i++) {
-        if (links[i].href.includes("napli.ru")) {
+        if (links[i].href.includes(site)) {
             let link = links[i];
             let nextGooglePage = false;
             console.log("Найдена строка " + links[i]);
             setTimeout(() => {
                 link.click();
-            }, getRandom(1000, 4000));
-    break;
+            }, getRandom(2500, 4500));
+            break;
+        }
+ }
 }
-}
-    // Ограничение поиска 5-й страницей:
-    if (document.querySelector(".YyVfkd").innerText == "5") {
-        let nextGooglePage = false;
-        location.href = "https://www.google.com";
-}
-    // Переход на следующую страницу поиска с заданным интервалом времени:
-    if (nextGooglePage) {
-        setTimeout(() => {
-            pnnext.click();
-        }, getRandom(2000, 4500));
-    }
-}
-
-
 // Функция рандома ключевых слов запроса:
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
+//Доступ к куки – через регулярные выражения.
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
